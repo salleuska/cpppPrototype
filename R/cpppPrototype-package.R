f3c088497c07ac1305a6040abe17c9606ff64a00
Paganin <paganin.1@osu.edu>
2025-11-10 15:51:13 -0300

    revis package declaration"

--- R/cpppPrototype-package.R
+++ R/cpppPrototype-package.R
@@ -1,8 +1,2 @@
-#' cpppPrototype: Calibration-Based Posterior Predictive p-values (Prototype)
-#'
-#' Infrastructure for calibrating posterior predictive p-values
-#' using NIMBLE o R functions.
-#'
-#' @docType package
-#' @name cpppPrototype
-NULL
+#' @keywords internal
+"_PACKAGE"
