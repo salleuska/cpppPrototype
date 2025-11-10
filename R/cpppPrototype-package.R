38a5fdba86b2baf035fde51d0c1b935acb5ccda0
Paganin <paganin.1@osu.edu>
2025-11-10 15:38:54 -0300

    added description, licensse and rignore files

---
+++ R/cpppPrototype-package.R
@@ -0,0 +1,8 @@
+#' cpppPrototype: Calibration-Based Posterior Predictive p-values (Prototype)
+#'
+#' Infrastructure for calibrating posterior predictive p-values
+#' using NIMBLE o R functions.
+#'
+#' @docType package
+#' @name cpppPrototype
+NULL
